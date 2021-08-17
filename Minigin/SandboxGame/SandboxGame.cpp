a3754f4bb82a71b5fe291be82a7edd35c31410ba
Nasus <MrNasus.B@gmail.com>
2021-08-17 17:11:33 +0200

    removed commented code

--- Minigin/SandboxGame/SandboxGame.cpp
+++ Minigin/SandboxGame/SandboxGame.cpp
@@ -45,20 +45,4 @@ void SandboxGame::Load()
 	t.SetPosition(Position2D{ 80.f, 20.f });
 	o->SetTransform(t);
 	scene.Add(o);
-
-
-	//std::shared_ptr<GameObject> go = std::make_shared<GameObject>();
-	//go->SetTexture("background.jpg");
-	//scene.Add(go);
-
-	//go = std::make_shared<GameObject>();
-	//go->SetTexture("logo.png");
-	//go->SetPosition(216, 180);
-	//scene.Add(go);
-
-	//auto font = ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
-	//auto to = std::make_shared<TextObject>("Programming 4 Assignment", font);
-	//to->SetPosition(80, 20);
-	//scene.Add(to);
-
 }
